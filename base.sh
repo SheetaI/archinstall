9181bca1b55676ba69287729250579ae22c17abb
SheetaI <im.kawaii.gg@gmail.com>
2022-04-18 06:36:47 +0800

    Update base.sh

--- base.sh
+++ base.sh
@@ -6,14 +6,25 @@ timedatectl set-ntp true
 
 echo -e "o\ny\nn\n \n \n+100M\nef00\nn\n \n \n+4G\n8200\nn\n \n \n \n8300\nw\ny" | gdisk /dev/vda
 
-mkfs.fat -F32 /dev/sda1		
-mkswap /dev/sda2			
-swapon /dev/sda2			
-mkfs.ext4 /dev/sda3
+# For Physical Machine #
+# mkfs.fat -F32 /dev/sda1		
+# mkswap /dev/sda2			
+# swapon /dev/sda2			
+# mkfs.ext4 /dev/sda3
 
-mount /dev/sda3 /mnt
+# mount /dev/sda3 /mnt
+# mkdir /mnt/boot
+# mount /dev/sda1 /mnt/boot
+
+# For Virtual Machine #
+mkfs.fat -F32 /dev/vda1		
+mkswap /dev/vda2			
+swapon /dev/vda2			
+mkfs.ext4 /dev/vda3
+
+mount /dev/vda3 /mnt
 mkdir /mnt/boot
-mount /dev/sda1 /mnt/boot
+mount /dev/vda1 /mnt/boot
 
 pacstrap /mnt base base-devel linux linux-firmware vim intel-ucode
 genfstab -U /mnt >> /mnt/etc/fstab
