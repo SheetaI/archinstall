c464e1a5472f3cacca39352e330d77af2edf65cf
SheetaI <im.kawaii.gg@gmail.com>
2022-04-18 07:38:04 +0800

    Update base.sh

--- base.sh
+++ base.sh
@@ -1,31 +1,36 @@
 # == MY ARCH SETUP INSTALLER == #
 
+# Configurations #
 sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 15/" /etc/pacman.conf
 loadkeys us
 timedatectl set-ntp true
 
+# Partitions: 100MB BOOT, 4GB SWAP, remaining ROOT #
 echo -e "o\ny\nn\n \n \n+100M\nef00\nn\n \n \n+4G\n8200\nn\n \n \n \n8300\nw\ny" | gdisk /dev/vda
 
-# For Physical Machine #
+# Format: Physical Machine #
 # mkfs.fat -F32 /dev/sda1		
 # mkswap /dev/sda2			
 # swapon /dev/sda2			
 # mkfs.ext4 /dev/sda3
 
+# Mount: Physical Machine #
 # mount /dev/sda3 /mnt
 # mkdir /mnt/boot
 # mount /dev/sda1 /mnt/boot
 
-# For Virtual Machine #
+# Format: Virtual Machine #
 mkfs.fat -F32 /dev/vda1		
 mkswap /dev/vda2			
 swapon /dev/vda2			
 mkfs.ext4 /dev/vda3
 
+# Mount: Virtual Machine #
 mount /dev/vda3 /mnt
 mkdir /mnt/boot
 mount /dev/vda1 /mnt/boot
 
+# Installation #
 pacstrap /mnt base base-devel linux linux-firmware vim intel-ucode
 genfstab -U /mnt >> /mnt/etc/fstab
 
