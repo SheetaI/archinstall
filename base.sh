8a17b4dbd2821416173d22a18deba45d7b18abc3
SheetaI <im.kawaii.gg@gmail.com>
2022-04-18 19:41:37 +0800

    Update base.sh

--- base.sh
+++ base.sh
@@ -9,26 +9,26 @@ timedatectl set-ntp true
 echo -e "o\ny\nn\n \n \n+100M\nef00\nn\n \n \n+4G\n8200\nn\n \n \n \n8300\nw\ny" | gdisk /dev/vda
 
 # Format: Physical Machine #
-# mkfs.fat -F32 /dev/sda1		
-# mkswap /dev/sda2			
-# swapon /dev/sda2			
-# mkfs.ext4 /dev/sda3
+mkfs.fat -F32 /dev/sda1		
+mkswap /dev/sda2			
+swapon /dev/sda2			
+mkfs.ext4 /dev/sda3
 
 # Mount: Physical Machine #
-# mount /dev/sda3 /mnt
-# mkdir /mnt/boot
-# mount /dev/sda1 /mnt/boot
+mount /dev/sda3 /mnt
+mkdir /mnt/boot
+mount /dev/sda1 /mnt/boot
 
 # Format: Virtual Machine #
-mkfs.fat -F32 /dev/vda1		
-mkswap /dev/vda2			
-swapon /dev/vda2			
-mkfs.ext4 /dev/vda3
+# mkfs.fat -F32 /dev/vda1		
+# mkswap /dev/vda2			
+# swapon /dev/vda2			
+# mkfs.ext4 /dev/vda3
 
 # Mount: Virtual Machine #
-mount /dev/vda3 /mnt
-mkdir /mnt/boot
-mount /dev/vda1 /mnt/boot
+# mount /dev/vda3 /mnt
+# mkdir /mnt/boot
+# mount /dev/vda1 /mnt/boot
 
 # Installation #
 pacstrap /mnt base base-devel linux linux-firmware vim intel-ucode
