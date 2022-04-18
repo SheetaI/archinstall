94e2f9ffa4780123e31535dd9faa7f288779246b
SheetaI <im.kawaii.gg@gmail.com>
2022-04-19 07:50:27 +0800

    Update base3.sh

--- base3.sh
+++ base3.sh
@@ -27,15 +27,31 @@ sudo ln -s /usr/share/fontconfig/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/con
 
 cd $HOME
 mkdir -p Pictures/Screenshots
-mkdir -p .config/{bspwm,sxhkd}
-install -Dm755 /usr/share/doc/bspwm/examples/bspwmrc .config/bspwm/
-install -Dm644 /usr/share/doc/bspwm/examples/sxhkdrc .config/sxhkd/
-sed -i "s/^urxvt$/st/" $HOME/.config/sxhkd/sxhkdrc
-cp /etc/X11/xinit/xinitrc $HOME/.xinitrc
-echo "exec bspwm" >> $HOME/.xinitrc
-chmod +x $HOME/.xinitrc
+
 sudo groupadd -r autologin
 sudo gpasswd -a sheetal autologin
+
+cat <<EOF > .bashrc
+# alias sourcing
+if [ -f ~/.bash_aliases ]; then
+	.  ~/.bash_aliases
+fi
+
+neofetch
+powerline-daemon -q
+POWERLINE_BASH_CONTINUATION=1
+POWERLINE_BASH_SELECT=1
+. /usr/share/powerline/bindings/bash/powerline.sh
+EOF'
+
+echo "alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'" >> .bash_aliases
+bash
+echo "dotfiles" >> .gitignore
+git clone --bare https://github.com/SheetaI/dotfiles $HOME/dotfiles
+alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
+config checkout
+
+
 
 # Enable services #
 sudo systemctl enable lightdm
