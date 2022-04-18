aaaeddbb16e5c10273b94212d339e20bbca553ad
SheetaI <im.kawaii.gg@gmail.com>
2022-04-19 06:36:48 +0800

    Update base3.sh

--- base3.sh
+++ base3.sh
@@ -1,35 +1,3 @@
-sudo pacman -S --noconfirm sed
-sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 15/" /etc/pacman.conf
-
-# Install Essentials #
-sudo pacman -S --noconfirm xorg xorg-xinit  lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings \
- networkmanager git bspwm sxhkd rofi feh lxsession lxappearance thunar ranger picom \
- firefox mpd mpc ncmpcpp udisks2 udiskie thunar-archive-plugin file-roller dunst \
- gedit htop libreoffice bleachbit maim pulseaudio pulseaudio-alsa alsa alsa-utils \
- powerline powerline-fonts youtube-dl galculator ueberzug sxiv gnome-disk-utility \
- qbittorrent gimp veracrypt obsidian neofetch firewalld
-
-# Start Network Service #
-sudo systemctl enable NetworkManager	
-sudo systemctl firewalld
-
-# AUR Helper #
-printf '\033c'
-cd $HOME
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-
-# St terminal #
-cd $HOME
-git clone https://github.com/siduck/st.git
-cd st
-sudo make install
-
-# AUR Packages #
-cd $HOME
-yay -S polybar ani-cli timeshift cava ttf-unifont ttf-symbola otf-symbola libxft-bgra-git brave-bin binance
-
 # Font Rendering #
 sudo cat <<EOF > /etc/fonts/local.conf
 <?xml version="1.0"?>
@@ -70,7 +38,7 @@ sudo groupadd -r autologin
 sudo gpasswd -a sheetal autologin
 
 # Enable services #
-sudo systemctl lightdm
+sudo systemctl enable lightdm
 
 ## End ##
 exit
