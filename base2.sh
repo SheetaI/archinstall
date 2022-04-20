dfaf002ab22fccf18b5a04a3a29e7b293a935a1e
SheetaI <im.kawaii.gg@gmail.com>
2022-04-20 21:22:40 +0800

    Update base2.sh

--- base2.sh
+++ base2.sh
@@ -26,15 +26,6 @@ default arch-*
 EOF
 
 # For Physical Machine #
-cat <<EOF > /boot/loader/entries/arch.conf
-title    Arch Linux
-linux    /vmlinuz-linux
-initrd   /intel-ucode.img
-initrd   /initramfs-linux.img
-options  root=/dev/sda3 rw
-EOF
-
-# For Testing UUID #
 UUID="$(lsblk -dno UUID /dev/sda3)"
 cat <<EOF > /boot/loader/entries/arch.conf
 title    Arch Linux
@@ -45,15 +36,16 @@ options root=UUID=$UUID rw
 EOF
 
 # For Virtual Machine #
-# cat <<EOF > /boot/loader/entries/arch.conf
-# title    Arch Linux
-# linux    /vmlinuz-linux
-# initrd   /intel-ucode.img
-# initrd   /initramfs-linux.img
-# options  root=/dev/vda3 rw
-# EOF
+#UUID="$(lsblk -dno UUID /dev/vda3)"
+#cat <<EOF > /boot/loader/entries/arch.conf
+#title    Arch Linux
+#linux    /vmlinuz-linux
+#initrd   /intel-ucode.img
+#initrd   /initramfs-linux.img
+#options root=UUID=$UUID rw
+#EOF
 
-# Start Network Service #
+# Startup Service #
 systemctl enable NetworkManager	
 
 # User & Password Setup #
