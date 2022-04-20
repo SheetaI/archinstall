bdc758f65a45b7835b5bc1716b47c8de88d08d8b
SheetaI <im.kawaii.gg@gmail.com>
2022-04-21 06:50:32 +0800

    Add files via upload

---
+++ base4.sh
@@ -0,0 +1,151 @@
+#!/bin/bash
+
+# == PERSONALIZATION == #
+# curl -JLO https://raw.githubusercontent.com/SheetaI/archinstall/master/base3.sh
+
+sudo pacman -Syu
+
+# Install Needed Pkgs #
+sudo pacman -S --noconfirm xorg xorg-xinit  lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings \
+ neofetch firewalld bspwm sxhkd rofi feh lxsession lxappearance thunar ranger picom \
+ firefox mpd mpc ncmpcpp udisks2 udiskie thunar-archive-plugin file-roller dunst \
+ gedit htop libreoffice bleachbit maim pulseaudio pulseaudio-alsa alsa alsa-utils \
+ powerline powerline-fonts youtube-dl galculator ueberzug sxiv gnome-disk-utility \
+ qbittorrent gimp veracrypt obsidian bash-completion sed go
+
+# AUR Helper #
+cd $HOME
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+# St terminal #
+cd $HOME
+git clone https://github.com/siduck/st.git
+cd st
+sudo make install
+
+# AUR Pkgs #
+cat <<EOF > $HOME/pkgs.txt
+polybar
+ani-cli-git
+timeshift
+cava
+ttf-unifont
+ttf-symbola
+otf-symbola
+libxft-bgra-git
+EOF
+
+cat <<EOF > $HOME/aurpkgs.sh
+#!/bin/bash
+
+# Check whether the file is provided
+if [[ $# -eq 0 ]]; then
+    echo 'Usage: ./aurpkgs.sh <file>'
+    exit 0
+fi
+
+# Check whether the file exists
+if [[ ! -f "$1" ]]; then
+    echo "$1: No such file"
+    exit 1
+fi
+
+pkgs=""
+
+# Loop through the text file line by line
+while IFS= read -r pkg; do
+    # Append the package name to pkgs
+    pkgs="$pkgs $pkg"
+done < "$1"
+
+# Install the packages
+yay -S $pkgs
+EOF
+
+chmod +x $HOME/aurpkgs.sh
+echo | ./aurpkgs.sh pkgs.txt
+
+# Font Rendering #
+sudo bash -c 'cat <<EOF > /etc/fonts/local.conf
+<?xml version="1.0"?>
+<!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+<fontconfig>
+  <match target="font">
+    <edit name="antialias" mode="assign">
+      <bool>true</bool>
+    </edit>
+    <edit name="hinting" mode="assign">
+      <bool>true</bool>
+    </edit>
+    <edit mode="assign" name="rgba">
+      <const>rgb</const>
+    </edit>
+    <edit mode="assign" name="hintstyle">
+      <const>hintslight</const>
+    </edit>
+    <edit mode="assign" name="lcdfilter">
+      <const>lcddefault</const>
+    </edit>
+  </match>
+</fontconfig>
+EOF'
+sudo ln -s /usr/share/fontconfig/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d/
+
+# Autologin #
+sudo groupadd -r autologin
+sudo gpasswd -a sheetal autologin
+sudo sed -i "s/^#autologin-user=$/autologin-user=sheetal/" /etc/lightdm/lightdm.conf
+sudo sed -i "s/^#autologin-user-timeout=0$/autologin-user-timeout=0/" /etc/lightdm/lightdm.conf
+
+# Ricing #
+cd $HOME
+mkdir Music
+mkdir -p Pictures/Screenshots
+
+# Ricing #
+sudo bash -c 'cat <<EOF > .bashrc
+# alias sourcing
+if [ -f ~/.bash_aliases ]; then
+	.  ~/.bash_aliases
+fi
+
+neofetch
+powerline-daemon -q
+POWERLINE_BASH_CONTINUATION=1
+POWERLINE_BASH_SELECT=1
+. /usr/share/powerline/bindings/bash/powerline.sh
+EOF'
+
+# Dotfiles Bare Repo #
+cd $HOME
+echo "alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'" >> .bash_aliases
+echo "dotfiles" >> .gitignore
+git clone --bare https://github.com/SheetaI/dotfiles.git $HOME/dotfiles
+alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
+
+# remove duplicates before checkout
+rm .bash_aliases
+rm .bashrc
+rm -rf .config/*
+rm -rf .config
+
+# config checkout
+/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME checkout
+# config config --local status.showUntrackedFiles no
+/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no
+
+# Make executables #
+cd $HOME/.ncmpcpp/ncmpcpp-ueberzug
+chmod +x ncmpcpp_cover_art.sh
+chmod +x ncmpcpp-ueberzug
+
+# Enable services #
+cd $HOME
+sudo systemctl enable lightdm
+sudo systemctl enable firewalld
+
+## End ##
+echo "Setup is done.. Please reboot the system"
+
