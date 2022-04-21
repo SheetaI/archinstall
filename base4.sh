440ee66d51553447cd99c3365922c745c20eb884
SheetaI <im.kawaii.gg@gmail.com>
2022-04-21 08:22:12 +0800

    Update base4.sh

--- base4.sh
+++ base4.sh
@@ -39,33 +39,6 @@ brave-bin
 binance
 EOF
 
-cat <<EOF > $HOME/aurpkgs.sh
-#!/bin/bash
-
-# Check whether the file is provided
-if [[ $# -eq 0 ]]; then
-    echo 'Usage: ./aurpkgs.sh <file>'
-    exit 0
-fi
-
-# Check whether the file exists
-if [[ ! -f "$1" ]]; then
-    echo "$1: No such file"
-    exit 1
-fi
-
-pkgs=""
-
-# Loop through the text file line by line
-while IFS= read -r pkg; do
-    # Append the package name to pkgs
-    pkgs="$pkgs $pkg"
-done < "$1"
-
-# Install the packages
-yay -S $pkgs
-EOF
-
 chmod +x $HOME/aurpkgs.sh
 echo | ./aurpkgs.sh pkgs.txt
 
