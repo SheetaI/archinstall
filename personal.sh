d1ba7eb1d7eb10efb77e06841db2f30f53f5b149
SheetaI <im.kawaii.gg@gmail.com>
2022-04-22 00:55:37 +0800

    Update personal.sh

--- personal.sh
+++ personal.sh
@@ -11,12 +11,12 @@ sudo bash -c "echo -e '\n[multilib]\nInclude = /etc/pacman.d/mirrorlist' >>/etc/
 sudo pacman -Syyu
 
 # Pkgs #
-pkgs="$("xorg" "xorg-xinit" "lightdm" "lightdm-gtk-greeter" "lightdm-gtk-greeter-settings" "neofetch" "firewalld" "bspwm" "sxhkd" "rofi" "feh" "lxsession" "lxappearance" "thunar" "ranger" "picom"
+pkgs="$('"xorg" "xorg-xinit" "lightdm" "lightdm-gtk-greeter" "lightdm-gtk-greeter-settings" "neofetch" "firewalld" "bspwm" "sxhkd" "rofi" "feh" "lxsession" "lxappearance" "thunar" "ranger" "picom"
 "firefox" "mpd" "mpc" "ncmpcpp" "udisks2" "udiskie" "thunar-archive-plugin" "file-roller" "dunst" "gedit" "htop" "libreoffice" "bleachbit" "maim" "pulseaudio" "pulseaudio-alsa" "alsa" "alsa-utils"
-"powerline" "powerline-fonts" "youtube-dl" "galculator" "ueberzug" "sxiv" "gnome-disk-utility" "qbittorrent" "gimp" "veracrypt" "obsidian" "bash-completion")"
+"powerline" "powerline-fonts" "youtube-dl" "galculator" "ueberzug" "sxiv" "gnome-disk-utility" "qbittorrent" "gimp" "veracrypt" "obsidian" "bash-completion"')"
 
 # Install Needed Pkgs #
-while ! pacman -Syw $pkgs; do
+while ! pacman -Syw ${pkgs}; do
   sleep 10
 done
 pacman -Su ${pkgs}
