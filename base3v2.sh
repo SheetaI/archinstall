47540f5c4ed53e6bd507f0085c58c46c2b35153e
SheetaI <im.kawaii.gg@gmail.com>
2022-04-21 21:15:59 +0800

    Create base3v2.sh

---
+++ base3v2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# == PERSONALIZATION == #
+# curl -JLO https://raw.githubusercontent.com/SheetaI/archinstall/master/base3.sh
+
+sudo sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 15/" /etc/pacman.conf
+sudo pacman -Syu
+
+# AUR Helper #
+cd $HOME
+git clone https://aur.archlinux.org/yay.git && cd yay
+yes | makepkg -si
+
+# St terminal #
+cd $HOME
+git clone https://github.com/siduck/st.git
+cd st
+sudo make install
+
+# AUR Pkgs #
+cd $HOME
+echo && yay -S --noconfirm --removemake --cleanafter polybar ani-cli-git timeshift cava ttf-unifont ttf-symbola otf-symbola libxft-bgra-git brave-bin binance
+
+cd $HOME/archinstall
+chmod +x base4.sh
+./base4.sh
