e134058af0f1b887a278f810d2533b72f5e4b638
SheetaI <im.kawaii.gg@gmail.com>
2022-04-22 00:51:18 +0800

    Update personal.sh

--- personal.sh
+++ personal.sh
@@ -10,13 +10,23 @@ sudo reflector --verbose --country Singapore,Taiwan,Indonesia,Thailand --latest 
 sudo bash -c "echo -e '\n[multilib]\nInclude = /etc/pacman.d/mirrorlist' >>/etc/pacman.conf"
 sudo pacman -Syyu
 
+# Pkgs #
+pkgs="xorg" "xorg-xinit" "lightdm" "lightdm-gtk-greeter" "lightdm-gtk-greeter-settings" "neofetch" "firewalld" "bspwm" "sxhkd" "rofi" "feh" "lxsession" "lxappearance" "thunar" "ranger" "picom"
+"firefox" "mpd" "mpc" "ncmpcpp" "udisks2" "udiskie" "thunar-archive-plugin" "file-roller" "dunst" "gedit" "htop" "libreoffice" "bleachbit" "maim" "pulseaudio" "pulseaudio-alsa" "alsa" "alsa-utils"
+"powerline" "powerline-fonts" "youtube-dl" "galculator" "ueberzug" "sxiv" "gnome-disk-utility" "qbittorrent" "gimp" "veracrypt" "obsidian" "bash-completion"
+
 # Install Needed Pkgs #
-sudo pacman -S --noconfirm xorg xorg-xinit  lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings \
- neofetch firewalld bspwm sxhkd rofi feh lxsession lxappearance thunar ranger picom \
- firefox mpd mpc ncmpcpp udisks2 udiskie thunar-archive-plugin file-roller dunst \
- gedit htop libreoffice bleachbit maim pulseaudio pulseaudio-alsa alsa alsa-utils \
- powerline powerline-fonts youtube-dl galculator ueberzug sxiv gnome-disk-utility \
- qbittorrent gimp veracrypt obsidian bash-completion
+while ! pacman -Syw ${pkgs}; do
+  sleep 10
+done
+pacman -Su ${pkgs}
+
+#sudo pacman -S --noconfirm xorg xorg-xinit  lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings \
+# neofetch firewalld bspwm sxhkd rofi feh lxsession lxappearance thunar ranger picom \
+# firefox mpd mpc ncmpcpp udisks2 udiskie thunar-archive-plugin file-roller dunst \
+# gedit htop libreoffice bleachbit maim pulseaudio pulseaudio-alsa alsa alsa-utils \
+# powerline powerline-fonts youtube-dl galculator ueberzug sxiv gnome-disk-utility \
+# qbittorrent gimp veracrypt obsidian bash-completion
 
 # St terminal #
 cd $HOME
