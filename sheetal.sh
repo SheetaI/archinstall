fd8cccd0d1c48af7a1b01b569c3b8206f2e915ed
SheetaI <im.kawaii.gg@gmail.com>
2022-04-22 22:02:38 +0800

    Update sheetal.sh

--- sheetal.sh
+++ sheetal.sh
@@ -16,7 +16,7 @@ sudo pacman -Syyu
 echo "-------------------------------------------------"
 echo "     Installing Pacman Pkgs"
 echo "-------------------------------------------------"
-pkg_list="xorg xorg-xinit lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings neofetch firewalld bspwm sxhkd rofi feh lxsession lxappearance thunar ranger picom firefox mpd mpc ncmpcpp udisks2 udiskie thunar-archive-plugin file-roller dunst gedit htop maim pulseaudio pulseaudio-alsa alsa alsa-utils powerline powerline-fonts youtube-dl ueberzug sxiv"
+pkg_list="xorg xorg-xinit lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings neofetch firewalld bspwm sxhkd rofi feh lxsession lxappearance thunar ranger picom firefox mpd mpc ncmpcpp udisks2 udiskie thunar-archive-plugin file-roller dunst gedit libreoffice galculator htop maim pulseaudio pulseaudio-alsa alsa alsa-utils powerline powerline-fonts youtube-dl ueberzug sxiv qbittorrent gimp veracrypt obsidian bleachbit"
 
 while ! sudo pacman -Syuw --noconfirm ${pkg_list}; do
   sleep 10
