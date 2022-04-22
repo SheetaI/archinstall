8f2e0ac37c50cdd877fbda2642ede4f602a03909
SheetaI <im.kawaii.gg@gmail.com>
2022-04-23 04:11:17 +0800

    Update base.sh

--- base.sh
+++ base.sh
@@ -7,6 +7,7 @@ echo "-------------------------------------------------"
 echo "-------------------------------------------------"
 echo "     Prepairing"
 echo "-------------------------------------------------"
+sleep 3
 sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 15/" /etc/pacman.conf
 pacman -Sy --noconfirm archlinux-keyring
 loadkeys us
@@ -18,22 +19,16 @@ echo "     Partitioning Drive"
 echo "-------------------------------------------------"
 echo " 100MB=BOOT | 4GB=SWAP | Remaing=ROOT"
 echo "-------------------------------------------------"
-
-# Partition: For Physican Machine #
-yes | echo -e "o\ny\nn\n \n \n+100M\nef00\nn\n \n \n+4G\n8200\nn\n \n \n \n8300\nw\ny" | gdisk /dev/sda
 sleep 3
+yes | echo -e "o\ny\nn\n \n \n+100M\nef00\nn\n \n \n+4G\n8200\nn\n \n \n \n8300\nw\ny" | gdisk /dev/sda
 clear 
-
-# Partition: For Virtual Machine #
-#yes | echo -e "o\ny\nn\n \n \n+100M\nef00\nn\n \n \n+4G\n8200\nn\n \n \n \n8300\nw\ny" | gdisk /dev/vda
-#sleep 3
-#sclear 
 
 echo "-------------------------------------------------"
 echo "     Formatting Physical Drive"
 echo "-------------------------------------------------"
 echo " FAT32=BOOT | LINUX SWAP=SWAP | LINUX FILESYSTEM=ROOT"
 echo "-------------------------------------------------"
+sleep 3
 mkfs.fat -F32 /dev/sda1		
 mkswap /dev/sda2			
 swapon /dev/sda2			
@@ -44,46 +39,28 @@ echo "     Mounting Physical Drive"
 echo "-------------------------------------------------"
 echo " /mnt/boot=BOOT | /=ROOT"
 echo "-------------------------------------------------"
+sleep 3
 mount /dev/sda3 /mnt
 mkdir /mnt/boot
 mount /dev/sda1 /mnt/boot
-
-
-#echo "-------------------------------------------------"
-#echo "     Formatting Virtual Drive"
-#echo "-------------------------------------------------"
-#echo " FAT32=BOOT | LINUX SWAP=SWAP | LINUX FILESYSTEM=ROOT"
-#echo "-------------------------------------------------"
-#mkfs.fat -F32 /dev/vda1		
-#mkswap /dev/vda2			
-#swapon /dev/vda2			
-#mkfs.ext4 /dev/vda3
-
-#echo "-------------------------------------------------"
-#echo "     Mounting Virtual Drive"
-#echo "-------------------------------------------------"
-#echo " /mnt/boot=BOOT | /=ROOT"
-#echo "-------------------------------------------------"
-#mount /dev/vda3 /mnt
-#mkdir /mnt/boot
-#mount /dev/vda1 /mnt/boot
-sleep 3
+clear
 
 echo "-------------------------------------------------"
 echo "    Installing Base System"
 echo "-------------------------------------------------"
+sleep 3
 basepkgs="base base-devel linux linux-firmware vim intel-ucode"
 
 while ! pacstrap /mnt ${basepkgs}; do
   sleep 10
 done
 pacstrap -U /mnt ${basepkgs}
-sleep 3
 clear
 
 echo "-------------------------------------------------"
 echo "    Generating fstab"
 echo "-------------------------------------------------"
+sleep 3
 genfstab -U /mnt >> /mnt/etc/fstab
 cp base2.sh /mnt
 cp sheetal.sh /mnt
@@ -91,6 +68,7 @@ cp sheetal.sh /mnt
 echo "-------------------------------------------------"
 echo "    Proceeding installation"
 echo "-------------------------------------------------"
+sleep 3
 chmod +x /mnt/base2.sh
 arch-chroot /mnt /bin/bash -c ./base2.sh
 
